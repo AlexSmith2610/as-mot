05821a5db0e00a5ce1c855d79795b8b4c52211d0
KLM <91488137+planeklm@users.noreply.github.com>
2023-09-12 18:01:11 +0100

    Added lua 54, updated version and added ox_lib.

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,10 +3,12 @@ game "gta5"
 
 author 'decaffeinatedtea'
 description 'MOT script made by Alex Smith with help from Jaysee'
-version 'v0.1.1'
+version 'v0.1.2'
+lua54 'yes'
 
 shared_scripts {
     'config.lua',
+    '@ox_lib/init.lua'
 }
 
 client_scripts {
